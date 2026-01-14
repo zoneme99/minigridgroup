c78837268c01fb08eeaf4b0ad274055ae9ec695f
Jonatan Cervall <jonatancervall@gmail.com>
2026-01-14 13:58:20 +0100

    added reward policy wrapper function

--- ctf_env.py
+++ ctf_env.py
@@ -154,6 +154,37 @@ class CaptureTheFlagPZ(ParallelEnv):
 
         return observations
 
+    def reward_policy(self, agent_id):
+        if agent_id not in actions:
+            continue
+
+        action = actions[agent_id]
+        self.env.agent_pos = self.agent_pos[agent_id]
+        self.env.agent_dir = self.agent_dir[agent_id]
+        self.env.step(action)
+        self.agent_pos[agent_id] = self.env.agent_pos
+        self.agent_dir[agent_id] = self.env.agent_dir
+
+        # --- NEW CAPTURE LOGIC ---
+        current_pos = tuple(self.env.agent_pos)
+        enemy = "blue" if agent_id == "red" else "red"
+        enemy_flag_loc = self.flag_pos[enemy]
+        my_base_loc = self.flag_pos[agent_id]
+        
+        # 1. PICKUP
+        if current_pos == enemy_flag_loc and not self.carrying_flag[agent_id]:
+            self.carrying_flag[agent_id] = True
+            self.env.grid.set(*enemy_flag_loc, Floor(enemy))
+            rewards[agent_id] += 1.0  # Bonus for picking up
+    
+        # 2. RETURN & WIN
+        if current_pos == my_base_loc and self.carrying_flag[agent_id]:
+            rewards[agent_id] += 10.0  # Victory!
+            for a in self.agents:
+                terminations[a] = True
+        
+        
+    
     def step(self, actions):
         rewards = {a: -0.01 for a in self.agents}
         terminations = {a: False for a in self.agents}
@@ -163,35 +194,7 @@ class CaptureTheFlagPZ(ParallelEnv):
         self.steps += 1
 
         for agent_id in self.agents:
-            if agent_id not in actions:
-                continue
-
-            action = actions[agent_id]
-            self.env.agent_pos = self.agent_pos[agent_id]
-            self.env.agent_dir = self.agent_dir[agent_id]
-
-            self.env.step(action)
-
-            self.agent_pos[agent_id] = self.env.agent_pos
-            self.agent_dir[agent_id] = self.env.agent_dir
-
-            # --- NEW CAPTURE LOGIC ---
-            current_pos = tuple(self.env.agent_pos)
-            enemy = "blue" if agent_id == "red" else "red"
-            enemy_flag_loc = self.flag_pos[enemy]
-            my_base_loc = self.flag_pos[agent_id]
-
-            # 1. PICKUP
-            if current_pos == enemy_flag_loc and not self.carrying_flag[agent_id]:
-                self.carrying_flag[agent_id] = True
-                self.env.grid.set(*enemy_flag_loc, Floor(enemy))
-                rewards[agent_id] += 1.0  # Bonus for picking up
-
-            # 2. RETURN & WIN
-            if current_pos == my_base_loc and self.carrying_flag[agent_id]:
-                rewards[agent_id] += 10.0  # Victory!
-                for a in self.agents:
-                    terminations[a] = True
+            self.reward_policy(agent_id)
 
         if self.steps >= self.max_steps:
             for a in self.agents:
